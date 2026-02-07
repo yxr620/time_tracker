cf615b2c213144debfd2a83c619a25bda119a412
yxr620 <yxr620@126.com>
2026-02-07 15:01:04 +0800

    feat: 添加 iOS 平台支持

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.timetracker',
+  appId: 'com.yxr.timetracker',
   appName: '时间追踪工具',
   webDir: 'dist',
   android: {
